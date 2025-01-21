e1d3ebdc9037ec822a335c2549c6d7dec1f77334
Santosh <s.aryasomayajula@7digilabs.com>
2025-01-21 22:04:10 +0530

    Base Project

---
+++ Source/TimberWork/TimberWork.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TimberWork.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimberWork, "TimberWork" );
